c2e7c48dd8ef52977d6f5557942a8321f0e2158a
Grace <lunay0325@gmail.com>
2019-02-25 05:37:54 +0800

    Add teacher's notification in assginLeader and appendMemeber method

--- src/Class.java
+++ src/Class.java
@@ -26,6 +26,7 @@ public class Class {
     public void assignLeader(Student leader) {
         if (leader.getStdClass() == this) {
             this.leader = leader;
+            this.teacher.welcomLeader(leader);
         } else {
             System.out.println("It is not one of us");
         }
@@ -33,7 +34,7 @@ public class Class {
 
     public void appendMember(Student std) {
         std.setStdClass(this);
-
+        this.teacher.welcomeStudent(std);
     }
 
     public boolean isIn(Student std) {

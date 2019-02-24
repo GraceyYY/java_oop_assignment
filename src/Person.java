1266d22809cb319fd44ced8bfac3b3c5e6c9b09f
Grace <lunay0325@gmail.com>
2019-02-25 04:04:49 +0800

    Implement the Person class

---
+++ src/Person.java
@@ -0,0 +1,39 @@
+public class Person {
+    private String id;
+    private String name;
+    private int age;
+
+    public Person(String id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

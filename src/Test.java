c1eb6723e905c2f97f722b737a612de3001fde50
Grace <lunay0325@gmail.com>
2019-02-25 05:58:00 +0800

    Create some instance to test the code

---
+++ src/Test.java
@@ -0,0 +1,36 @@
+public class Test {
+    public static void main(String[] args) {
+        Class c1 = new Class(1);
+        Class c2 = new Class(2);
+        Class c3 = new Class(3);
+        Class c4 = new Class(4);
+
+        Student s1 = new Student("123", "jack", 16);
+        Student s2 = new Student("234", "jerry", 17);
+        Student s3 = new Student("384", "ann", 16);
+        Student s4 = new Student("598", "ben", 17);
+        Student s5 = new Student("098", "carol", 16);
+        Student s6 = new Student("273", "lily", 17);
+
+        Class[] cl1 = {c1, c2};
+        Class[] cl2 = {c3, c4};
+
+        Teacher t1 = new Teacher("389", "cris", 30, cl1);
+        Teacher t2 = new Teacher("726", "lee", 29, cl2);
+        c1.setTeacher(t1);
+        c2.setTeacher(t1);
+        c3.setTeacher(t2);
+        c4.setTeacher(t2);
+
+
+        c1.appendMember(s1);
+        c1.assignLeader(s1);
+        c1.assignLeader(s2);
+        c2.appendMember(s3);
+        c3.appendMember(s4);
+        c3.assignLeader(s4);
+
+        System.out.println(s1.introduce());
+        System.out.println(t1.introduce());
+    }
+}
